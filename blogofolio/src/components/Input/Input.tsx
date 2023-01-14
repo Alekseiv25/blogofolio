4fc0185b7ac9cac8190f7b0e56fdc9b090e28984
Alekseiv25 <alekseiv25@gmail.com>
2023-01-14 16:09:35 +0300

    change: ts es2015, add: formdata

--- blogofolio/src/components/Input/Input.tsx
+++ blogofolio/src/components/Input/Input.tsx
@@ -2,13 +2,15 @@ import styles from './Input.module.scss'
 
 interface InputProps {
     type: 'email' | 'password' | 'text'
-    name: string
     label: string
     placeholder: string
+    name: string
 }
 
+//Что в onChange писать вместо any
+
 export const Input = (props: InputProps) => {
-    const { label, type, name, placeholder } = props
+    const { label, type, placeholder, name } = props
 
     return (
         <>
--- blogofolio/src/components/SignIn/SignIn.tsx
+++ blogofolio/src/components/SignIn/SignIn.tsx
@@ -1,20 +1,70 @@
+import { useState } from 'react'
 import { Input } from '../Input/Input'
 import { Navigation } from '../Navigaton/Navigation'
 import { Submit } from '../Submit/Submit'
 import styles from './SignIn.module.scss'
 
 export const SignIn = () => {
+    const submit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        const formData = new FormData(event.currentTarget)
+        event.preventDefault()
+        for (let [key, value] of formData.entries()) {
+            console.log(key, value)
+        }
+    }
     return (
         <>
             <Navigation backToHome='Back to home' text={'Sign In'} />
-            <form className={styles.Formwrapper}>
-                <Input type={'email'} name={''} label={'Email'} placeholder={'Your Email'} />
-                <Input type={'password'} name={''} label={'Password'} placeholder={'Your password'} />
+            <form onSubmit={submit} className={styles.Formwrapper}>
+                <Input
+                    type='email'
+                    label='Email'
+                    placeholder='Your Email'
+                    name='email'
+                // value={signInData.email}
+                // onChange={changeLogin}
+                />
+                <Input
+                    type='password'
+                    label='Password'
+                    placeholder='Your password'
+                    name='password'
+                // value={signInData.password}
+                // onChange={changePassword}
+                />
                 <a href='#!' className={styles.passwordtext}>Forgot password?</a>
-                <Submit value={'Sign in'} />
+                <Submit value='Sign in' />
                 <p>Don't have an account? <a href="#!">Sign up</a></p>
             </form>
         </>
     )
 }
 
+
+
+
+
+
+    // const [signInData, setSignInData] = useState({
+    //     email: '',
+    //     password: ''
+    // })
+
+    // const changeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const currentValue = e.target.value
+    //     setSignInData({
+    //         ...signInData,
+    //         email: currentValue
+    //     })
+    // }
+
+    // const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const currentValue = e.target.value
+    //     setSignInData({
+    //         ...signInData,
+    //         password: currentValue
+    //     })
+    // }
+
+    // const clickForm = (e: any) => {
+    //     console.log(e)
