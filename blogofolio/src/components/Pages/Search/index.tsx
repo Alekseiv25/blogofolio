2f414b1cebeb242af7028e8361f70a58bebe7bfd
Alekseiv25 <alekseiv25@gmail.com>
2023-01-28 16:28:21 +0300

    black theme

--- blogofolio/src/components/Pages/Search/index.tsx
+++ blogofolio/src/components/Pages/Search/index.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react"
-import {  GetSearchPosts, IPost } from "../../Services/PostService"
+import { useContext, useEffect, useState } from "react"
+import { GetSearchPosts, IPost } from "../../Services/PostService"
 import { SearchPost } from "../../SearchPost"
 import styles from './Search.module.scss'
+import { ThemeContext } from "../../Layout"
 
 
 
 export const Search = () => {
+    const { themeColor } = useContext(ThemeContext)
     const [asidePost, setAsidePost] = useState<IPost[]>([])
     useEffect(() => {
         GetSearchPosts(1).then(post => { setAsidePost(post) })
@@ -13,7 +15,7 @@ export const Search = () => {
     return (
 
         <section>
-            <input className={styles.input} type='text'  value={'Search result: 1'} />
+            <input className={`${styles.input} ${themeColor === 'dark' ? `${styles.dark}` : ''}`} type='text' value={'Search result: 1'} />
             <div >
                 {asidePost.map((el) => (
                     <SearchPost key={el.id} {...el} />
--- blogofolio/src/components/SearchPost/index.tsx
+++ blogofolio/src/components/SearchPost/index.tsx
@@ -1,15 +1,17 @@
 import { IPost } from '../Services/PostService'
 import style from './SearchPost.module.scss'
 import { PostButtons } from '../PostButtons'
-
+import { useContext } from 'react'
+import { ThemeContext } from '../Layout'
 export const SearchPost = (props: IPost) => {
     const { date, title, image } = props
+    const { themeColor } = useContext(ThemeContext)
     return (<div className={style.post}>
         <div className={style.container}>
             <img className={style.image} src={image} alt='123'></img>
             <div className={style.titlewrapper}>
                 <p className={style.date}>{date}</p>
-                <p className={style.title}>{title} </p>
+                <p className={`${style.title} ${themeColor === 'dark' ? `${style.dark}` : ''}`}>{title} </p>
             </div>
 
         </div>
