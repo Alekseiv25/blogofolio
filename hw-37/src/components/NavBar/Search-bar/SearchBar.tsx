84c35a9761bc13cfee6c9dd6424d2933df5c051f
Alekseiv25 <alekseiv25@gmail.com>
2023-04-01 18:40:48 +0300

    refactor

--- hw-37/src/components/NavBar/Search-bar/SearchBar.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './SearchBar.module.scss'
-
-export const SearchBar = () => {
-    return (<input type='search' className={styles.input} placeholder='Search...' ></input>)
-}
--- hw-37/src/components/NavBar/Search-button/SearchButton.tsx
+++
@@ -1,6 +0,0 @@
-import styles from './SearchButton.module.scss'
-import SearchIco from './search-ico.svg'
-
-export const SearchButton = () => {
-    return (<button className={styles.button} placeholder='Search...' ><img src={SearchIco} alt="search-ico" /></button>)
-}
